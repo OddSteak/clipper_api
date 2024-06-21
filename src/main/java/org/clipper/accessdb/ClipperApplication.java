a4ee88ed5e64b20d0eb1ce651db4482324d7a160
OddSteak <sedentaryowl@riseup.net>
2024-06-22 04:03:31 +0800

    test commit

---
+++ src/main/java/org/clipper/accessdb/ClipperApplication.java
@@ -0,0 +1,13 @@
+package org.clipper.accessdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClipperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClipperApplication.class, args);
+	}
+
+}
---
+++ src/main/java/org/clipper/accessdb/User.java
@@ -0,0 +1,7 @@
+package org.clipper.accessdb;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
