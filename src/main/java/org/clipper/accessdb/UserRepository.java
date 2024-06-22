e32209cb6f812387e358e5328f7e90233117d17f
OddSteak <sedentaryowl@riseup.net>
2024-06-22 18:49:58 +0800

    no errors!

---
+++ src/main/java/org/clipper/accessdb/UserRepository.java
@@ -0,0 +1,10 @@
+package org.clipper.accessdb;
+
+import org.springframework.data.repository.CrudRepository;
+import org.clipper.accessdb.User;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface UserRepository extends CrudRepository<User, Integer> { }
+
